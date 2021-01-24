5ff50a5457458dee90749f7ed5b1bfe635f4944a
jenPlaza <jpplaza@student.fullsail.edu>
2021-01-23 23:20:01 -0500

    fixed blog margins

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,17 +10,32 @@ import Grid from '@material-ui/core/Grid';
 //style
 const useStyles = makeStyles((theme) => ({
   root: {
-    // border: '1px solid blue',
+    //border: '1px solid red',
+  },
+  container: {
+    //border: '1px solid blue',
     textAlign: 'center',
+    marginTop: '25%',
+    [theme.breakpoints.up('sm')]: {
+      marginTop: '0%',
+    },
   },
   h1: {
     fontSize: '2.5em',
+    margin: 'auto',
+    marginTop: '15%',
     fontFamily: 'Zapfino',
+    [theme.breakpoints.up('sm')]: {
+      marginTop: '5%',
+    },
   },
   blog: {
     padding: '1%',
-    marginTop: '0%',
+    marginTop: '10%',
     marginBottom: '65%',
+    [theme.breakpoints.up('sm')]: {
+      marginTop: '5%',
+    },
     [theme.breakpoints.up('xl')]: {
       //marginTop: '25%',
     },
@@ -30,12 +45,13 @@ const useStyles = makeStyles((theme) => ({
 export default function Blog() {
   const classes = useStyles();
   return (
-    <div className={classes.root} style={styles.container}>
-      <h1 className={classes.h1}>Blog</h1>
-      <Grid container spacing={0}>
+    <div className={classes.root} style={styles.bkg}>
+      <Grid container spacing={0} className={classes.container}>
+        <h1 className={classes.h1}>Blog</h1>
+
         <Grid item xs={12} className={classes.blog}>
           <hr />
-          <h2>Comming Soon</h2>
+          <h2>Coming Soon</h2>
         </Grid>
       </Grid>
     </div>
@@ -43,7 +59,7 @@ export default function Blog() {
 }
 
 const styles = {
-  container: {
+  bkg: {
     backgroundImage: 'url(' + imgUrl + ')',
     //backgroundRepeat: 'no-repeat',
     margin: '0% 0% 0% 0%',
